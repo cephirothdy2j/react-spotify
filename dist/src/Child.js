68b8d847e6c8a31bb2cf261c3b0efbe9b1392265
Dylan Schuster <dylan.schuster@zipscene.com>
2015-12-14 11:44:07 -0500

    removing unused files

--- dist/src/Child.js
+++
@@ -1,9 +0,0 @@
-var Child = React.createClass({displayName: "Child",
-  render: function(){
-    return (
-      React.createElement("div", null, 
-        "and this is the ", React.createElement("b", null, this.props.name), "."
-      )
-    )
-  }
-});
