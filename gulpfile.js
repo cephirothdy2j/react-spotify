3e6a2fb80c0fabbc4350875af67175532139df95
Dylan Schuster <dylan.schuster@zipscene.com>
2015-12-14 11:44:59 -0500

    Gulp build process updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,56 +1,48 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var react = require('gulp-react');
 var htmlreplace = require('gulp-html-replace');
-var source = require('vinyl-source-stream');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var reactify = require('reactify');
-var streamify = require('gulp-streamify');
+var less = require('gulp-less');
+var pathPack = require('path');
 
 var path = {
   HTML: 'src/index.html',
+  ALL: ['src/less/*.less','src/js/*.js', 'src/js/**/*.js', 'src/index.html'],
+  JS: ['src/js/*.js', 'src/js/**/*.js'],
+  CSS: 'src/less/*.less',
   MINIFIED_OUT: 'build.min.js',
-  OUT: 'build.js',
-  DEST: 'dist',
-  DEST_BUILD: 'dist/build',
   DEST_SRC: 'dist/src',
-  ENTRY_POINT: './src/js/App.js'
+  DEST_BUILD: 'dist/build',
+  DEST: 'dist'
 };
+
+gulp.task('transform', function(){
+  gulp.src(path.JS)
+    .pipe(react())
+    .pipe(gulp.dest(path.DEST_SRC));
+});
 
 gulp.task('copy', function(){
   gulp.src(path.HTML)
     .pipe(gulp.dest(path.DEST));
 });
 
-gulp.task('watch', function() {
-  gulp.watch(path.HTML, ['copy']);
-
-  var watcher  = watchify(browserify({
-    entries: [path.ENTRY_POINT],
-    transform: [reactify],
-    debug: true,
-    cache: {}, packageCache: {}, fullPaths: true
-  }));
-
-  return watcher.on('update', function () {
-    watcher.bundle()
-      .pipe(source(path.OUT))
-      .pipe(gulp.dest(path.DEST_SRC))
-      console.log('Updated');
-  })
-    .bundle()
-    .pipe(source(path.OUT))
+gulp.task('less', function () {
+  return gulp.src('src/less/bootstrap.less')
+    .pipe(less())
     .pipe(gulp.dest(path.DEST_SRC));
 });
 
+gulp.task('watch', function(){
+  gulp.watch(path.ALL, ['less','transform', 'copy']);
+});
+
 gulp.task('build', function(){
-  browserify({
-    entries: [path.ENTRY_POINT],
-    transform: [reactify],
-  })
-    .bundle()
-    .pipe(source(path.MINIFIED_OUT))
-    .pipe(streamify(uglify(path.MINIFIED_OUT)))
+  gulp.src(path.JS)
+    .pipe(react())
+    .pipe(concat(path.MINIFIED_OUT))
+    .pipe(uglify(path.MINIFIED_OUT))
     .pipe(gulp.dest(path.DEST_BUILD));
 });
 
@@ -62,6 +54,6 @@ gulp.task('replaceHTML', function(){
     .pipe(gulp.dest(path.DEST));
 });
 
-gulp.task('production', ['replaceHTML', 'build']);
-
 gulp.task('default', ['watch']);
+
+gulp.task('production', ['replaceHTML', 'build']);
